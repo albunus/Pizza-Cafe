b3a10d0628425cba7044c9843b05a1634bbb1890
albunus <albunus.nyalita@student.moringaschool.com>
2021-09-20 19:58:08 +0300

    update my files

--- index.js
+++ index.js
@@ -1,86 +1,108 @@
+function OrderList(type, crust, toppings, numberOfPizza, delivery, size) {
+    this.type = type;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.numberOfPizza = numberOfPizza;
+    this.delivery = delivery;
+    this.size = size;
+    this.sizePrice = 0;
+    this.crustPrice = 0;
+    this.toppingPrice = 0;
+    this.deliveryPrice = 0;
+    this.price = 0;
+};
+
+//adding new property to object with a method to calculate prices
+OrderList.prototype.finalPrice = function () {
+    if (this.size === "small") {
+        this.sizePrice = 500;
+    } else if (this.size === "medium") {
+        this.sizePrice = 800;
+    } else if (this.size === "large") {
+        this.sizePrice = 1000;
+    };
+    if (this.crust === "flat bread") {
+        this.crustPrice = 150;
+    } else if (this.crust === "thin") {
+        this.crustPrice = 80;
+    } else if (this.crust === "stuffed") {
+        this.crustPrice = 100;
+    } else if (this.crust === "cheese") {
+        this.crustPrice = 200;
+    };
+    if (this.toppings === "sausages") {
+        this.toppingsPrice = 150;
+    } else if (this.toppings === "onions") {
+        this.toppingsPrice = 80;
+    } else if (this.toppings === "mushroom") {
+        this.toppingsPrice = 100;
+    } else if (this.toppings === "cheese") {
+        this.toppingsPrice = 200;
+    };
+    this.price = (this.sizePrice + this.crustPrice + this.toppingsPrice) * this.numberOfPizza;
+
+};
+OrderList.prototype.makeDelivery = function () {
+    if (this.delivery === "deliver") {
+        this.price += 150;
+    } else if (this.delivery === "pick") {
+        this.price += 0;
+    };
+};
 
 
-    // //////////////////////////////////////////////order form activities/////////////////////////////
+function resetFieldValues() {
+    $("#size").val("");
+    $("#type").val("");
+    $("#toppings").val("");
+    $("#crust").val("");
+    $("#quantity").val("");
+    $("#name").val("");
+    $("#address").val("");
+};
 
-    function Order(name,email,number,type,crust,toppings,quantity,size,address) {
-        this.name = name;
-        this.email = email;
-        this.number =number;
-        this.type = type;
-        this.crust = crust;
-        this.topping = toppings;
-        this.quantity = quantity;
-        this.size = size;
-        this.address = address;
-      };
-      Order.prototype.totalPrice = function () {
-        if (this.size === "small") {
-            this.sizePrice = 500;
-        } else if (this.size === "medium") {
-            this.sizePrice = 800;
-        }else if (this.size === "large") {
-            this.sizePrice = 1000;
-        };
-        if (this.crust === "flat bread") {
-            this.crustPrice = 100;
-        } else if (this.crust === "stuffed") {
-            this.crustPrice = 150;
-        }else if (this.crust === "Cheese") {
-            this.crustPrice = 200;
-        }else if (this.crust === "thin") {
-          this.crustPrice = 80;
-        };
-        if (this.toppings === "Sausage.") {
-            this.toppingPrice = 100;
-        }else if (this.toppings === "onions") {
-            this.toppingPrice = 80;
-        }else if (this.toppings === "Meat.") {
-            this.toppingPrice = 200;
-        }else if (this.toppings === "mushrooms") {
-            this.toppingPrice = 150;
-        };
-        this.Price =(this.sizePrice +this.crustPrice + this.toppingPrice) * this.quantity;
-      };
-      
-      ///////////////////////////determine prices when delivered
+$(document).ready(function () {
+    $(".order").submit(function (event) {
+        event.preventDefault();
+        var size = $("#size").val();
+        var type = $("#type").val();
+        var crust = $("#crust").val();
+        var toppings = $("#toppings").val();
+        var numberOfPizza = parseInt($("#quantity").val());
+        var delivery = $("#delivery").val();
+        var newOrderList = new OrderList(type, crust, toppings, numberOfPizza, delivery, size);
+        newOrderList.finalPrice();
 
-      $(document).ready(function () {
-        $('.gallery').click(function () {
-            $('#gallery').show();
-            $("#aboutUs").hide();
-            $('#order').hide();
-        });
-        $('.aboutUs').click(function () {
-            $('#aboutUs').show();
-            $("#gallery").hide();
-            $('#order').hide();
-        });
-        $('.btn1').click(function () {
-            $('#order').show();
-            $("#gallery").hide();
-            $("#aboutUs").hide();
-        });
-    
-    $("#form").submit(function (event) {
-    event.preventDefault();
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var type = $("#one").find("option:selected").val();
-        var size = $("#two").find("option:selected").val();
-        var toppings = $("#three").find("option:selected").val();
-        var crust = $("#four").find("option:selected").val();
-        var quantity = $("#five").val();
-        var number = $("#number").val();
-        var address = $("#address").val();
-        var newOrder = new Order(name,email,number,type,crust,toppings,quantity,size,address)
-        newOrder.totalPrice();
-        alert("Cost is"+" "+totalPrice);
-        alert(quantity);
-        alert(number);
+        newOrderList.makeDelivery();
+        $("#tush").text("You have ordered " + numberOfPizza + " " + size + " " + type + " pizza(s) with a  " + crust + " crust and  " + toppings + " topping");
+        $("#output").text("YOUR BILL IS " + newOrderList.price + " /= ." + " For Delivery fill the delivery form");
+        // resetFieldValues();
+
     });
-    // $("#output").click(function(){
-    //     return totalPrice;
-    // })
 
+    $("#checkOut").click(function (event) {
+        event.preventDefault();
+        var inputtedName = $("#name").val();
+        var inputtedaddress = $("#address").val();
+        window.alert("Hey" + " " + inputtedName + " " + " Your order will be ready in 20mins and will be delivered to" + " " + inputtedaddress + " " + "in the next 30 mins.")
+        window.alert("Your total order will be" + newOrderList.price + delivery);
+
+        resetFieldValues();
+    });
+
+    $('.gallery').click(function () {
+        $('#gallery').show();
+        $("#aboutUs").hide();
+        $('#order').hide();
+    });
+    $('.aboutUs').click(function () {
+        $('#aboutUs').show();
+        $("#gallery").hide();
+        $('#order').hide();
+    });
+    $('.btn1').click(function () {
+        $('#order').show();
+        $("#gallery").hide();
+        $("#aboutUs").hide();
+    });
 });
-
